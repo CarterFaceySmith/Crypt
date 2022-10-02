dfb98bc34def60f0f920a70908b5e4bcf0fb1bfd
Carter Facey-Smith <43100930+CarterFaceySmith@users.noreply.github.com>
2022-10-02 21:46:58 +1100

    poetry setup and dep update

--- src/enCrypt.py
+++ src/enCrypt.py
@@ -1,6 +1,3 @@
-# Set pubKey via server set password and hash
-# When user connects with correct password, send them publicKey for encrypt and decrypt and set in their code
-
 # Imports
 import random, sys, os, rabinMiller, theMath
 
